eec60fd17661a84f4e6ec36175a87f84b2c31d84
Youssouf El Azizi <youssoufelazizi@gmail.com>
2018-10-22 16:57:27 +0100

    update markdown examples

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -8,8 +8,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'React Popup Component',
+        short_name: 'React Popup',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
