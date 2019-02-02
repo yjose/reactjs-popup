6e7a8665c63594491120feebb3b25fb98681a48f
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-02-02 23:43:04 +0100

    update eslint config with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: ["airbnb", "prettier", "prettier/react"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      impliedStrict: true,
+      classes: true
+    }
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        trailingComma: "all",
+        bracketSpacing: false,
+        jsxBracketSameLine: true
+      }
+    ],
+    "import/no-named-as-default": 0,
+    "import/prefer-default-export": 0,
+    "import/no-unresolved": 0,
+    "import/no-named-as-default": 0,
+    "import/no-unresolved": 0,
+    "no-case-declarations": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/extensions": 0,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    "react/no-danger": 0,
+    "react/prop-types": 0,
+    'no-plusplus': 0,
+    "react/forbid-prop-types":0,
+    "global-require":0,
+    "no-return-assign":0
+  }
+};
+
+// inspired by wes bos config
+//https://www.youtube.com/watch?v=YIvjKId9m2c
--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import React from "react";
-import { findDOMNode } from "react-dom";
-import calculatePosition from "./Utils";
+import React from 'react';
+import calculatePosition from './Utils';
+import Ref from './Ref';
 
-import styles from "./index.css.js";
+import styles from './index.css.js';
+
 const POSITION_TYPES = [
-  "top left",
-  "top center",
-  "top right",
-  "right top",
-  "right center",
-  "right bottom",
-  "bottom left",
-  "bottom center",
-  "bottom right",
-  "left top",
-  "left center",
-  "left bottom"
+  'top left',
+  'top center',
+  'top right',
+  'right top',
+  'right center',
+  'right bottom',
+  'bottom left',
+  'bottom center',
+  'bottom right',
+  'left top',
+  'left center',
+  'left bottom',
 ];
 
 export default class Popup extends React.PureComponent {
   static defaultProps = {
-    children: () => <span> Your Content Here !!</span>,
+    // children: () => <span> Your Content Here !!</span>,
     trigger: null,
     onOpen: () => {},
     onClose: () => {},
@@ -28,14 +29,14 @@ export default class Popup extends React.PureComponent {
     open: false,
     disabled: false,
     closeOnDocumentClick: true,
-    repositionOnResize:true,
+    repositionOnResize: true,
     closeOnEscape: true,
-    on: ["click"],
+    on: ['click'],
     contentStyle: {},
     arrowStyle: {},
     overlayStyle: {},
-    className: "",
-    position: "bottom center",
+    className: '',
+    position: 'bottom center',
     modal: false,
     lockScroll: false,
     arrow: true,
@@ -43,12 +44,7 @@ export default class Popup extends React.PureComponent {
     offsetY: 0,
     mouseEnterDelay: 100,
     mouseLeaveDelay: 100,
-    keepTooltipInside: false
-  };
-  state = {
-    isOpen: this.props.open || this.props.defaultOpen,
-    modal: this.props.modal ? true : !this.props.trigger
-    // we create this modal state because the popup can't be a tooltip if the trigger prop doesn't exist
+    keepTooltipInside: false,
   };
 
   constructor(props) {
@@ -57,26 +53,36 @@ export default class Popup extends React.PureComponent {
     this.setContentRef = r => (this.ContentEl = r);
     this.setArrowRef = r => (this.ArrowEl = r);
     this.setHelperRef = r => (this.HelperEl = r);
+    this.setOverlayRef = r => (this.OverlayEl = r);
+    const {open, modal, defaultOpen, trigger} = props;
+
+    this.state = {
+      isOpen: open || defaultOpen,
+      modal: modal ? true : !trigger,
+      // we create this modal state because the popup can't be a tooltip if the trigger prop doesn't exist
+    };
+
     this.timeOut = 0;
   }
 
   componentDidMount() {
-    const { closeOnEscape, defaultOpen ,repositionOnResize} = this.props;
+    const {closeOnEscape, defaultOpen} = this.props;
     if (defaultOpen) this.setPosition();
     if (closeOnEscape) {
-      window.addEventListener("keyup", this.onEscape);
-    }
-    if(repositionOnResize){
-      window.addEventListener('resize', this.repositionOnResize);
+      /* eslint-disable-next-line no-undef */
+      window.addEventListener('keyup', e => {
+        if (e.key === 'Escape') this.closePopup();
+      });
     }
   }
 
   repositionOnResize = () => {
-      this.setPosition()
-  }
-  onEscape = (e) => {
-    if (e.key === "Escape") this.closePopup();
-  }
+    this.setPosition();
+  };
+
+  onEscape = e => {
+    if (e.key === 'Escape') this.closePopup();
+  };
 
   componentWillReceiveProps(nextProps) {
     if (this.props.open === nextProps.open) return;
@@ -85,10 +91,10 @@ export default class Popup extends React.PureComponent {
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.disabled !== this.props.disabled
-        && this.props.disabled
-        && this.state.isOpen) {
-        this.closePopup();
+    const {disabled} = this.props;
+    const {isOpen} = this.state;
+    if (prevProps.disabled !== disabled && disabled && isOpen) {
+      this.closePopup();
     }
   }
 
@@ -96,69 +102,94 @@ export default class Popup extends React.PureComponent {
     // kill any function to execute if the component is unmounted
     clearTimeout(this.timeOut);
 
-    const { closeOnEscape ,repositionOnResize} = this.props;
- // remove events listeners
+    const {closeOnEscape, repositionOnResize} = this.props;
+    // remove events listeners
     if (closeOnEscape) {
-      window.removeEventListener("keyup", this.onEscape);
+      window.removeEventListener('keyup', this.onEscape);
     }
-    if(repositionOnResize){
+    if (repositionOnResize) {
       window.removeEventListener('resize', this.repositionOnResize);
     }
   }
 
   lockScroll = () => {
     if (this.state.modal && this.props.lockScroll)
-      document.getElementsByTagName("body")[0].style.overflow = "hidden";
+      /* eslint-disable-next-line no-undef */
+      document.getElementsByTagName('body')[0].style.overflow = 'hidden';
   };
+
   resetScroll = () => {
     if (this.state.modal && this.props.lockScroll)
-      document.getElementsByTagName("body")[0].style.overflow = "auto";
+      /* eslint-disable-next-line no-undef */
+      document.getElementsByTagName('body')[0].style.overflow = 'auto';
   };
 
   togglePopup = () => {
     if (this.state.isOpen) this.closePopup();
     else this.openPopup();
   };
+
   openPopup = () => {
-    if (this.state.isOpen || this.props.disabled) return;
-    this.setState({ isOpen: true }, () => {
+    const {isOpen} = this.state;
+    const {onOpen, disabled} = this.props;
+
+    if (isOpen || disabled) return;
+    this.setState({isOpen: true}, () => {
       this.setPosition();
-      this.props.onOpen();
+      onOpen();
       this.lockScroll();
+      setTimeout(() => {
+        this.OverlayEl.style['transform-style'] = 'none';
+        this.ContentEl.style['transform-style'] = 'none';
+        // this.OverlayEl.classList.remove("popup-overlay");
+        // this.ContentEl.classList.remove("popup-content");
+      }, 1000);
     });
   };
+
   closePopup = () => {
-    if (!this.state.isOpen) return;
-    this.props.onClose();
-    this.setState({ isOpen: false }, () => {
-      this.resetScroll();
-    });
+    const {isOpen} = this.state;
+    if (isOpen) return;
+    const {onClose} = this.props;
+    this.OverlayEl.classList.add('popup-overlay-out');
+    this.ContentEl.classList.add('popup-content-out');
+    setTimeout(() => {
+      onClose();
+      this.setState({isOpen: false}, () => {
+        this.resetScroll();
+      });
+    }, 500);
   };
+
   onMouseEnter = () => {
     clearTimeout(this.timeOut);
-    const { mouseEnterDelay } = this.props;
+    const {mouseEnterDelay} = this.props;
     this.timeOut = setTimeout(() => this.openPopup(), mouseEnterDelay);
   };
+
   onMouseLeave = () => {
     clearTimeout(this.timeOut);
-    const { mouseLeaveDelay } = this.props;
+    const {mouseLeaveDelay} = this.props;
     this.timeOut = setTimeout(() => this.closePopup(), mouseLeaveDelay);
   };
 
   getTooltipBoundary = () => {
-    const { keepTooltipInside } = this.props;
+    const {keepTooltipInside} = this.props;
     let boundingBox = {
       top: 0,
       left: 0,
+      /* eslint-disable-next-line no-undef */
       width: window.innerWidth,
-      height: window.innerHeight
+      /* eslint-disable-next-line no-undef */
+      height: window.innerHeight,
     };
-    if (typeof keepTooltipInside === "string") {
+    if (typeof keepTooltipInside === 'string') {
+      /* eslint-disable-next-line no-undef */
       const selector = document.querySelector(keepTooltipInside);
-      if (process.env.NODE_ENV !== "production") {
+      if (process.env.NODE_ENV !== 'production') {
         if (selector === null)
           throw new Error(
-            `${keepTooltipInside} selector is not exist : keepTooltipInside must be a valid html selector 'class' or 'Id'  or a boolean value`
+            `${keepTooltipInside} selector is not exist : keepTooltipInside must be a valid html selector 'class' or 'Id'  or a boolean value`,
           );
       }
       boundingBox = selector.getBoundingClientRect();
@@ -167,9 +198,16 @@ export default class Popup extends React.PureComponent {
   };
 
   setPosition = () => {
-    const { modal,isOpen } = this.state;
-    if (modal|| !isOpen) return;
-    const { arrow, position, offsetX, offsetY, keepTooltipInside, arrowStyle } = this.props;
+    const {
+      arrow,
+      position,
+      offsetX,
+      offsetY,
+      keepTooltipInside,
+      arrowStyle,
+    } = this.props;
+    const {modal} = this.state;
+    if (modal) return;
     const helper = this.HelperEl.getBoundingClientRect();
     const trigger = this.TriggerEl.getBoundingClientRect();
     const content = this.ContentEl.getBoundingClientRect();
@@ -187,33 +225,35 @@ export default class Popup extends React.PureComponent {
       arrow,
       {
         offsetX,
-        offsetY
+        offsetY,
       },
-      boundingBox
+      boundingBox,
     );
-    this.ContentEl.style.top = cords.top - helper.top + "px";
-    this.ContentEl.style.left = cords.left - helper.left + "px";
+    this.ContentEl.style.top = `${cords.top - helper.top}px`;
+    this.ContentEl.style.left = `${cords.left - helper.left}px`;
     if (arrow) {
-      this.ArrowEl.style["transform"] = cords.transform;
-      this.ArrowEl.style["-ms-transform"] = cords.transform;
-      this.ArrowEl.style["-webkit-transform"] = cords.transform;
+      this.ArrowEl.style.transform = cords.transform;
+      this.ArrowEl.style['-ms-transform'] = cords.transform;
+      this.ArrowEl.style['-webkit-transform'] = cords.transform;
       this.ArrowEl.style.top = arrowStyle.top || cords.arrowTop;
       this.ArrowEl.style.left = arrowStyle.left || cords.arrowLeft;
     }
     if (
+      /* eslint-disable-next-line no-undef */
       window
         .getComputedStyle(this.TriggerEl, null)
-        .getPropertyValue("position") == "static" ||
+        .getPropertyValue('position') === 'static' ||
+      /* eslint-disable-next-line no-undef */
       window
         .getComputedStyle(this.TriggerEl, null)
-        .getPropertyValue("position") == ""
+        .getPropertyValue('position') === ''
     )
-      this.TriggerEl.style.position = "relative";
+      this.TriggerEl.style.position = 'relative';
   };
 
   addWarperAction = () => {
-    const { contentStyle, className, on } = this.props;
-    const { modal } = this.state;
+    const {contentStyle, className, on} = this.props;
+    const {modal} = this.state;
     const popupContentStyle = modal
       ? styles.popupContent.modal
       : styles.popupContent.tooltip;
@@ -224,73 +264,80 @@ export default class Popup extends React.PureComponent {
       ref: this.setContentRef,
       onClick: e => {
         e.stopPropagation();
-      }
+      },
     };
-    if (!modal && on.indexOf("hover") >= 0) {
+    if (!modal && on.indexOf('hover') >= 0) {
       childrenElementProps.onMouseEnter = this.onMouseEnter;
       childrenElementProps.onMouseLeave = this.onMouseLeave;
     }
     return childrenElementProps;
   };
+
   renderTrigger = () => {
-    const triggerProps = { key: "T" };
-    const { on, trigger } = this.props;
+    const triggerProps = {key: 'T'};
+    const {on, trigger} = this.props;
     const onAsArray = Array.isArray(on) ? on : [on];
     for (let i = 0, len = onAsArray.length; i < len; i++) {
       switch (onAsArray[i]) {
-        case "click":
+        case 'click':
           triggerProps.onClick = this.togglePopup;
           break;
-        case "hover":
+        case 'hover':
           triggerProps.onMouseEnter = this.onMouseEnter;
           triggerProps.onMouseLeave = this.onMouseLeave;
           break;
-        case "focus":
+        case 'focus':
           triggerProps.onFocus = this.onMouseEnter;
           break;
+        default:
       }
     }
 
-    if (typeof trigger === "function")
+    if (typeof trigger === 'function')
       return React.cloneElement(trigger(this.state.isOpen), triggerProps);
 
     return React.cloneElement(trigger, triggerProps);
   };
 
   renderContent = () => {
-    const { arrow, arrowStyle } = this.props;
-    const { modal } = this.state;
+    const {arrow, arrowStyle, children} = this.props;
+    const {modal, isOpen} = this.state;
     return (
       <div {...this.addWarperAction()} key="C">
-        {arrow &&
-          !modal && (
-            <div
-              ref={this.setArrowRef}
-              style={Object.assign({}, styles.popupArrow, arrowStyle)}
-            />
-          )}
-        {typeof this.props.children === "function"
-          ? this.props.children(this.closePopup, this.state.isOpen)
-          : this.props.children}
+        {arrow && !modal && (
+          <div
+            ref={this.setArrowRef}
+            style={Object.assign({}, styles.popupArrow, arrowStyle)}
+          />
+        )}
+        {typeof children === 'function'
+          ? children(this.closePopup, isOpen)
+          : children}
       </div>
     );
   };
 
   render() {
-    const { overlayStyle, closeOnDocumentClick, on } = this.props;
-    const { modal } = this.state;
-    const overlay = this.state.isOpen && !(on.indexOf("hover") >= 0);
+    const {
+      overlayStyle,
+      closeOnDocumentClick,
+      on,
+      className,
+      trigger,
+    } = this.props;
+    const {modal, isOpen} = this.state;
+    const overlay = isOpen && !(on.indexOf('hover') >= 0);
     const ovStyle = modal ? styles.overlay.modal : styles.overlay.tooltip;
     return [
-      !!this.props.trigger && (
+      !!trigger && (
         <Ref innerRef={this.setTriggerRef} key="R">
           {this.renderTrigger()}
         </Ref>
       ),
-      this.state.isOpen && (
+      isOpen && (
         <div
           key="H"
-          style={{ position: "absolute", top: "0px", left: "0px" }}
+          style={{position: 'absolute', top: '0px', left: '0px'}}
           ref={this.setHelperRef}
         />
       ),
@@ -300,18 +347,18 @@ export default class Popup extends React.PureComponent {
           className="popup-overlay"
           style={Object.assign({}, ovStyle, overlayStyle)}
           onClick={closeOnDocumentClick ? this.closePopup : undefined}
-        >
+          ref={this.setOverlayRef}>
           {modal && this.renderContent()}
         </div>
       ),
-      this.state.isOpen && !modal && this.renderContent(),
+      isOpen && !modal && this.renderContent(),
     ];
   }
 }
 
-if (process.env.NODE_ENV !== "production") {
-  const PropTypes = require("prop-types");
-  const TRIGGER_TYPES = ["hover", "click", "focus"];
+if (process.env.NODE_ENV !== 'production') {
+  const PropTypes = require('prop-types');
+  const TRIGGER_TYPES = ['hover', 'click', 'focus'];
 
   Popup.propTypes = {
     arrowStyle: PropTypes.object,
@@ -319,9 +366,11 @@ if (process.env.NODE_ENV !== "production") {
     overlayStyle: PropTypes.object,
     className: PropTypes.string,
     modal: PropTypes.bool,
+    arrow: PropTypes.bool,
     closeOnDocumentClick: PropTypes.bool,
-    repositionOnResize:PropTypes.bool,
+    repositionOnResize: PropTypes.bool,
     disabled: PropTypes.bool,
+    closeOnEscape: PropTypes.bool,
     lockScroll: PropTypes.bool,
     offsetX: PropTypes.number,
     offsetY: PropTypes.number,
@@ -334,29 +383,17 @@ if (process.env.NODE_ENV !== "production") {
     trigger: PropTypes.oneOfType([PropTypes.func, PropTypes.element]), // for uncontrolled component we don't need the trigger Element
     on: PropTypes.oneOfType([
       PropTypes.oneOf(TRIGGER_TYPES),
-      PropTypes.arrayOf(PropTypes.oneOf(TRIGGER_TYPES))
+      PropTypes.arrayOf(PropTypes.oneOf(TRIGGER_TYPES)),
     ]),
     children: PropTypes.oneOfType([
       PropTypes.func,
       PropTypes.element,
-      PropTypes.string
+      PropTypes.string,
     ]).isRequired,
     position: PropTypes.oneOfType([
       PropTypes.oneOf(POSITION_TYPES),
-      PropTypes.arrayOf(PropTypes.oneOf(POSITION_TYPES))
+      PropTypes.arrayOf(PropTypes.oneOf(POSITION_TYPES)),
     ]),
-    keepTooltipInside: PropTypes.oneOfType([PropTypes.bool, PropTypes.string])
+    keepTooltipInside: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
   };
-}
-
-class Ref extends React.PureComponent {
-  componentDidMount() {
-    const { innerRef } = this.props;
-    if (innerRef) innerRef(findDOMNode(this));
-  }
-
-  render() {
-    const { children } = this.props;
-    return React.Children.only(children);
-  }
 }
