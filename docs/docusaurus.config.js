014aa2baf37d1f0f75adec99285f4103cdf6b792
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2021-07-03 17:11:32 +0100

    docs: remove cacheTime option in sitemap

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -80,7 +80,6 @@ module.exports = {
           customCss: require.resolve('./src/css/custom.css'),
         },
         sitemap: {
-          cacheTime: 600 * 1000, // 600 sec - cache purge period
           changefreq: 'weekly',
           priority: 0.5,
         },
